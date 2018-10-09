e90c6db5691497c28f8616ff66c4695527fbedc8
Reza-Amani <reza.amani@gmail.com>
2018-10-09 21:50:38 +1300

    VS 2017 project

---
+++ BlackJack2/BlackJack2/Card.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "Card.h"
+
+
+Card::Card(Suits s, Ranks r)
+{
+	suit = s;
+	rank = r;
+//	rule = rule
+}
+
+int Card::get_value()//Irule& rule)
+{
+	return 0;//rule.card_value(this);
+}
+
+
+Card::~Card()
+{
+}
