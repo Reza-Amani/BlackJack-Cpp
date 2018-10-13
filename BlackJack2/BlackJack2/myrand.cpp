53400a530445130dbcc4b8612ed09102ea37a474
Reza-Amani <reza.amani@gmail.com>
2018-10-14 00:08:51 +1300

    funcs added to myrand. a linker problem in methods

--- BlackJack2/BlackJack2/myrand.cpp
+++ BlackJack2/BlackJack2/myrand.cpp
@@ -4,6 +4,21 @@
 
 template<class T> myrand<T>::myrand(T const min, T const max)
 {
+	std::random_device rd;
+	pmt = new std::mt19937(rd());
+	pdist = new std::uniform_int_distribution<T>(min, max);
+}
+
+template<class T> myrand<T>::myrand()
+{
+	std::random_device rd;
+	pmt = new std::mt19937(rd());
+	pdist = new std::uniform_int_distribution<bool>(0, 1);
+}
+
+template<class T> T myrand<T>::get_rnd()
+{
+	return pdist(*pmt);
 }
 
 
--- BlackJack2/BlackJack2/myrand.h
+++ BlackJack2/BlackJack2/myrand.h
@@ -1,8 +1,14 @@
 #pragma once
+#include <random>
 template<class T> class myrand
 {
+	std::mt19937* pmt;
+	std::uniform_int_distribution<T>* pdist;
 public:
 	myrand(T const min, T const max);
+	myrand();
+	myrand(float const min, float const max) = delete;
+	T get_rnd();
 	~myrand();
 };
 
