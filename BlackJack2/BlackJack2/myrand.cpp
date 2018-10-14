49369203fc0c40c052029f4fa581bedd60dcd89e
reza.amani <reza.amani@gmail.com>
2018-10-14 15:17:08 +1300

    template specialisation

--- BlackJack2/BlackJack2/myrand.cpp
+++ BlackJack2/BlackJack2/myrand.cpp
@@ -9,19 +9,22 @@ template<class T> myrand<T>::myrand(T const min, T const max)
 	pdist = new std::uniform_int_distribution<T>(min, max);
 }
 
-template<class T> myrand<T>::myrand()
-{
-	std::random_device rd;
-	pmt = new std::mt19937(rd());
-	pdist = new std::uniform_int_distribution<bool>(0, 1);
-}
-
 template<class T> T myrand<T>::get_rnd()
 {
 	return pdist(*pmt);
 }
 
 
-template<class T> myrand<T>::~myrand()
+template<> myrand<double>::myrand(double const min, double const max)
 {
+	std::random_device rd;
+	pmt = new std::mt19937(rd());
+	pdist = new std::uniform_double_distribution<double>(min, max);
 }
+
+template<> double myrand<double>::get_rnd()
+{
+	return pdist(*pmt);
+}
+
+
--- BlackJack2/BlackJack2/myrand.h
+++ BlackJack2/BlackJack2/myrand.h
@@ -6,9 +6,20 @@ template<class T> class myrand
 	std::uniform_int_distribution<T>* pdist;
 public:
 	myrand(T const min, T const max);
-	myrand();
 	myrand(float const min, float const max) = delete;
 	T get_rnd();
 	~myrand();
 };
+
+
+template<> class myrand <double>
+{
+	std::mt19937* pmt;
+	std::uniform_int_distribution<double>* pdist;
+public:
+	myrand(double const min, double const max);
+	double get_rnd();
+	~myrand();
+};
+
 
