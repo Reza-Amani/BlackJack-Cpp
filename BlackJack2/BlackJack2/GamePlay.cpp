d01f0e98c2321098ae9f53ca90a6320374f3079d
reza.amani <reza.amani@gmail.com>
2018-10-19 21:34:11 +1300

    passing std::function tested

--- BlackJack2/BlackJack2/GamePlay.cpp
+++ BlackJack2/BlackJack2/GamePlay.cpp
@@ -1,14 +1,25 @@
 #include "pch.h"
 #include "GamePlay.h"
+#include <iostream>
+#include <functional>
+
 
 
 GamePlay::GamePlay()
 {
 }
 
+result GamePlay::run(bool echo, std::function<int(void)> p_get_card)
+{
+	DealerFirst = p_get_card();
+	if(echo)
+		std::cout << DealerFirst;
+	return (result)0;
+}
+
 void GamePlay::TakeFirstDealerCard(Ranks r)
 {
-
+	DealerFirst = get_card();
 }
 void TakeFirstPlayerCards(Ranks r1, Ranks r2);
 
--- BlackJack2/BlackJack2/GamePlay.h
+++ BlackJack2/BlackJack2/GamePlay.h
@@ -1,15 +1,23 @@
 #pragma once
 #include "Card.h"
 #include <functional>
+enum result
+{
+	_LOSE,
+	_PUSH,
+	_WIN
+};
 class GamePlay
 {
 public:
-	std::function<int(void)> callback;
+	int DealerFirst, Delaer;
+	std::function<int(void)> get_card;
 	//func* GetGo_Nogo  --main decides if nogo at any threshold
 	//func* int GetCard()  --main decides if rand card or all combinations
 	//result,sum@Nogo, sum@firstpair, DealerFirst run();	//main will make 3D array of results and number of occcurances
 	GamePlay();
 	~GamePlay();
+	result run(bool echo, std::function<int(void) > p_get_card);
 	void TakeFirstDealerCard(Ranks r);
 	void TakeFirstPlayerCards(Ranks r1, Ranks r2);
 };
