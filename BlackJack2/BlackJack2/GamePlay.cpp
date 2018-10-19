d60a408ae903ba2f048e8df80470812f7041f9b2
reza.amani <reza.amani@gmail.com>
2018-10-20 09:59:20 +1300

    game completed in manual mode

--- BlackJack2/BlackJack2/GamePlay.cpp
+++ BlackJack2/BlackJack2/GamePlay.cpp
@@ -11,7 +11,7 @@ GamePlay::GamePlay()
 
 result GamePlay::run(bool echo, bool auto_player)
 {
-	int Dealer_First=0, Dealer_sum=0, Player_pair=0, Player_sum=0;
+	int Dealer_First=0, Dealer_sum=0, Player_pair=0, Player_sum=0, Player_cards_no=0;
 
 	if (echo) std::cout << "Dealer's first: ";
 	Dealer_First = get_card(); 
@@ -28,19 +28,46 @@ result GamePlay::run(bool echo, bool auto_player)
 	if (echo) std::cout << player_card << "\n";
 	Player_pair += player_card;
 	Player_sum = Player_pair;
+	if (Player_sum == 21)
+		return result::_WIN;
 
-	std::cout << "\n player cards: ";
+	Player_cards_no = 2;
+	if(echo) std::cout << "\n player cards: ";
 	while (Player_decision(auto_player, Dealer_First, Player_pair))
 	{
 		player_card = get_card();
+		Player_cards_no++;
 		if (echo) std::cout << player_card << " ";
 		Player_sum += player_card;
 		if (Player_sum > 21)
 			return result::_LOSE;
 		else if (Player_sum == 21)
 			return result::_WIN;
+		else if (Player_cards_no >= 5)
+			return result::_WIN;
 	}
-	return result::_PUSH;
+
+	if (echo) { std::cout << "\r\n Dealer's turn: "; }
+
+	int Dealer_card;
+	while (Dealer_sum< Player_sum)
+	{
+		Dealer_card = get_card();
+		if (echo) std::cout << Dealer_card << " ";
+		Dealer_sum += Dealer_card;
+	}
+	if (Dealer_sum > 22)
+		return result::_WIN;
+	else if (Dealer_sum == 22)
+		return result::_PUSH;
+	else if (Dealer_sum == 21)
+		return result::_LOSE;
+	else if (Dealer_sum > Player_sum)
+		return result::_LOSE;
+	else if (Dealer_sum == Player_sum)
+		return result::_PUSH;
+	else
+		return result::_WIN;
 }
 
 bool GamePlay::Player_decision(bool auto_player, int Dealer_First, int Player_pair)
